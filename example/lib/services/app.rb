9002b2e12c6c0a2a325bdcf5a98bcdff982579e1
Fernando Mumbach <fermuch@gmail.com>
2013-04-27 20:42:26 -0300

    redesigned to make it work again! :happy:

--- example/lib/services/app.rb
+++ example/lib/services/app.rb
@@ -1,24 +1,26 @@
 class Services::App < Service
   tag :web
+  version 6
 
-  def install      
-    services.ruby.install        
+  def install
+    services.ruby.install
+    
     apply_once :install do |box| 
       logger.info "installing :#{service_name}"
-      box.fake_bash "cd #{config.app_path} && git clone app"
+      services.ruby.install
+      # box.fake_bash "cd #{config.app_path} && git clone app"
     end
   end
 
   def update
     install        
     logger.info "updating :#{service_name}"
-    boxes.each{|box| box.fake_bash "cd #{config.app_path} && git pull app"}
+    # boxes.each{|box| box.fake_bash "cd #{config.app_path} && git pull app"}
   end
 
-  def deploy    
+  def deploy
     update
-    logger.info "deploying :#{service_name}"  
-    services.my_sql.started
-    services.thin.restart
-  end    
+    logger.info "deploying :#{service_name}"
+    # services.thin.restart
+  end
 end
--- example/lib/services/my_sql.rb
+++
@@ -1,7 +0,0 @@
-class Services::MySql < Service
-  tag :db
-  def started
-    logger.info "ensuring mysql is running"
-    box.fake_bash 'mysql start' unless box.fake_bash('ps -A') =~ /mysql/
-  end
-end
